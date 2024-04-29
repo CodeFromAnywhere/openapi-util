537ea9f74106dac2a1b545e704ba2fc370f6ddfa
Wijnand Karsens <wijnand@karsens.com>
2024-04-29 09:53:24 +0200

    improvements

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,9 +1,10 @@
+export * from "./fetchOpenapi.js";
 export * from "./getOperations.js";
+export * from "./makeOpenapiPathRouter.js";
 export * from "./openapi-types.js";
 export * from "./resolveOpenapiAppRequest.js";
 export * from "./resolveReferenceOrContinue.js";
 export * from "./resolveResource.js";
 export * from "./tryGetOperationBodySchema.js";
 export * from "./tryValidateSchema.js";
-export * from "./fetchOpenapi.js";
 //# sourceMappingURL=index.d.ts.map
